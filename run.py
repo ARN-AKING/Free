2c1cdbe67e78f7c368f34f2921688b13ca1a9685
Farhad Joya <96083971+AryanHack907@users.noreply.github.com>
2023-03-20 23:11:45 +0430

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__FKING__":
    try:
        os.system("git pull")
-       __import__("FKING").FKING()
+       __import__("FKING").cekk()
    except Exception as e:
        exit(str(e))
