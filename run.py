2829463a5433f1c276f2fe18571015de04fff958
Farhad Joya <96083971+AryanHack907@users.noreply.github.com>
2023-03-20 23:03:20 +0430

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os,platform
 os.system('git pull')
  
-trt=platform.architecture()[0]
+FKING=platform.architecture()[0]
 if FKING=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif FKING=="64bit":
