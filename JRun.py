1c46a9b312136371dcd2906c49d4867d2496e209
Farhad Joya <96083971+AryanHack907@users.noreply.github.com>
2023-03-20 22:46:50 +0430

    Update JRun.py

--- JRun.py
+++ JRun.py
@@ -1,6 +1,24 @@
-import os, sys
-os.system('git pull')
-try:
-    __import__("FKING").FKING()
-except Exception as e:
-    exit(str(e))
+import os
+
+ 
+
+if __name__ == "__FKING__":
+
+ 
+
+   try:
+
+ 
+
+       os.system("git pull")
+ 
+
+       __import__("FKING").makedirectory()
+
+ 
+
+   except Exception as e: 
+
+ 
+
+       exit(str(e))
