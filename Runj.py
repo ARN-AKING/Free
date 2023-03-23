37f2622954276764c3835a1603ed03533d7fbcb3
Farhad Joya <96083971+AryanHack907@users.noreply.github.com>
2023-03-23 20:24:53 +0430

    Update Runj.py

--- Runj.py
+++ Runj.py
@@ -1,15 +1,6 @@
 import os, platform
 os.system('git pull')
-os.system('pkg update')
-os.system('pkg upgrade')
-os.system('pip install future')
-os.system('pip install requests')
-os.system('pip install mechanize')
-os.system('pip install lolcat')
-os.system('pip install bs4')
-os.system('pkg install git')
 import requests
-os.system("xdg-open https://github.com/AryanHack907")
 bit = platform.architecture()[0]
 if bit == '64bit':
     from FKING import FKING
